5bf88d0e439e52d8b14af1b1aba3005cc484c57b
yashodha <hettiarachchi.yashodha@gmail.com>
2021-04-16 13:55:02 +0530

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {AppService} from './app.service';
+import { ParentComponent } from './parent/parent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ParentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [AppService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
