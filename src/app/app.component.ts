74dbaeed2bbc6eec9a17d37bfe13cf5026b5a50d
yd1722 <y.dominatz@gmail.com>
2021-05-25 23:59:22 +0530

    INT | HTTP Interceptors | Use default http interceptor and custom handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
-import { AppService } from './app.service';
+import { Component, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpInterceptor,
+  HttpRequest,
+  HttpXhrBackend,
+} from '@angular/common/http';
+import { HttpInterceptorHandler } from './interceptors/HttpInterceptorHandler';
+import { InterceptorOne, InterceptorTwo } from './interceptors/interceptors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'ng-start';
+  response_1: Observable<any> | undefined;
+  response_2: Observable<any> | undefined;
+  title = 'ng-playground';
+
+  constructor(
+    private backend: HttpXhrBackend,
+    private http: HttpClient,
+    @Inject(HTTP_INTERCEPTORS) private interceptors: HttpInterceptor[]
+  ) {}
+
+  request() {
+    const url = 'https://jsonplaceholder.typicode.com/posts/1';
+    this.response_1 = this.http.get(url, { observe: 'body' });
+  }
+
+  requestUsingCustomHandlers() {
+    const req = new HttpRequest(
+      'GET',
+      'https://jsonplaceholder.typicode.com/posts/1'
+    );
+
+    const handler_1 = new HttpInterceptorHandler(
+      this.backend,
+      new InterceptorOne()
+    );
+
+    const handler_2 = new HttpInterceptorHandler(
+      handler_1,
+      new InterceptorTwo()
+    );
+
+    this.response_2 = handler_2.handle(req);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppService } from './app.service';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { InterceptorTwo, InterceptorOne } from './interceptors/interceptors';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [AppService],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorOne,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorTwo,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
